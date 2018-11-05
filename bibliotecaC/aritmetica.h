917b06b091eabf6a02030865dcf0d6d00f3f21f1
SaludQ <saludqg10@gmail.com>
2018-11-05 11:49:20 +0100

    Bibliotecas dinámicas

---
+++ bibliotecaC/aritmetica.h
@@ -0,0 +1,12 @@
+// Biblioteca en C
+
+// Archivo de cabecera -especificación-.
+int suma (int sumando1, int sumando2);
+
+int resta  (int minuendo, int sustraendo);
+
+int multiplicacion (int  numero1, int numero2);
+
+float division (int dividendo, int divisor);
+
+
